61c9251aeae9ecd75ec3b013bc173c1e88e8e02b
MardonovD <mardonovdoston69@gmail.com>
2022-04-19 14:00:03 +0500

    react lesson1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //react bu standart  xolatda shunaqa yozish kerak..
+// tepadagi yozish bu componentalarni yozish uchun kerak
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from "react-dom"; //render qilish uchun kerak va index.html bilan ulab beradi...
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const data = function () {
+  let data = new Date();
+
+  const kun = data.getDate();
+  const soat = data.getHours();
+
+  const years = data.getFullYear();
+  const oy = data.getMonth();
+  const sekund = data.getSeconds();
+  return { kun, soat, oy, years, sekund };
+};
+
+const App = function () {
+  const malumot = data();
+
+  return (
+    <div>
+      <p>
+        {malumot.kun}:{malumot.oy}:{malumot.years}:{malumot.sekund}
+      </p>
+    </div>
+  );
+};
+
+setInterval(function () {
+  ReactDOM.render(<App />, document.querySelector("#root"));
+}, 1000);
+
+// reatct proyect yaratidhni ikki xil usuli mavjud.
+//1. npm install -g  create-react-app --> kutubxonasidan globalny qilib kompyuterimizning xamma joyiga ishlaydigan qilib install qilamaiz..
+
+// URTATISH--> npm install -g create-react-app (bir marta qilinadigan narsa bu c discda user papkasida saqlanadi..)
+
+//2. papka yaratish uchun --> (create-react-app NOMIProject) yozamiza....
+
+// react structurasi quyudagicha-->
+//1.public-> default xolatdagi  papkalar
+
+const Doston = () => {
+  const name = "doston";
+  const age = 20;
+  const isMerriad = false;
+  return { name, age, isMerriad };
+};
+
+console.log(Doston);
